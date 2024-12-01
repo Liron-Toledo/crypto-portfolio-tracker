a64a4e97d061a0a72b10cc7b1c3bd75da2321712
Liron-Toledo <liron.toledo@outlook.com>
2024-12-01 21:50:04 +0000

    Add Error Boundry around whole App

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AddEditHolding from './pages/AddEditHolding';
 import Details from './pages/Details';
+import ErrorBoundary from './components/ErrorBoundry';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/add" element={<AddEditHolding />} />
-        <Route path="/edit/:id" element={<AddEditHolding />} />
-        <Route path="/details/:id" element={<Details />} />
-      </Routes>
-    </Router>
+    <ErrorBoundary>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add" element={<AddEditHolding />} />
+          <Route path="/edit/:id" element={<AddEditHolding />} />
+          <Route path="/details/:id" element={<Details />} />
+        </Routes>
+      </Router>
+    </ErrorBoundary>
   );
 }
 
